c54610e99b1a276d1cacf3c284382af9936bf0a4
Munna <96653272+Munna764592@users.noreply.github.com>
2023-01-09 22:49:21 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,21 +1,23 @@
+import img5 from '../image/webiplay-high-resolution-logo-color-on-transparent-background.png'
+
 export default function Navbar() {
   return (
     <>
     <div className="header">
       <div className="container navigation">
-        <a href="#">
+        <a href="https://www.google.com/">
           <img
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAHXPluq6GtTRPDIHRv5kJPy86uFjp5sO7hg&usqp=CAU"
-            alt=""
+            src={img5}
+            alt="logo"
           />
         </a>
         <div className="nav-menu">
           <div className="menu-bar">
-            <a href="#" className="underline">
+            <a href="https://www.google.com/" className="underline">
               Home
             </a>
             <div className="menu-2">
-              <a href="#full-2" className="underline" id="my-service">
+              <a href="https://www.google.com/full-2" className="underline" id="my-service">
                 Services{" "}
                 <i className="fa-sharp fa-solid fa-caret-down my-serices" />
               </a>
@@ -23,38 +25,39 @@ export default function Navbar() {
             </div>
 
             <div className="pages">
-              <a href="#pages" className="underline" id="my-pages">
+              <a href="https://www.google.com/pages" className="underline" id="my-pages">
                 Pages <i className="fa-sharp fa-solid fa-caret-down" />
               </a>
               <div id="pages" >pages</div>
             </div>
             <div className="case-study">
-              <a href="#case-study" className="underline">
+              <a href="https://www.google.com/case-study" className="underline">
                 Case Study <i className="fa-sharp fa-solid fa-caret-down" />
               </a>
               <div id="case-study" >case study</div>
             </div>
             <div className="blog">
-              <a href="#" id="my-blog" className="underline">
+              <a href="https://www.google.com/" id="my-blog" className="underline">
                 Blog <i className="fa-sharp fa-solid fa-caret-down" />
               </a>
               <div id="blog" >blog</div>
             </div>
-            <a href="#" className="underline">
+            <a href="https://www.google.com/" className="underline">
               Contacts
             </a>
           </div>
         </div>
         {/* <!-- hamburger --> */}
-        <div className="hamburger">
-          <input type="checkbox" id="navi-toggle" className="checkbox" />
-          <label className="button">
+        <div>
+          <input id="checkbox" type="checkbox" className="checkbox" />
+          <label htmlFor="checkbox" className="button">
             <span className="icon">&nbsp;</span>
           </label>
           <div className="background">background</div>
         </div>
+        {/* header  */}
         <div className="nav-icon">
-          <a href="#">
+          <a href="https://www.google.com/">
             <span id="iconup" className=" material-symbols-outlined">
               shopping_bag
             </span>
@@ -72,7 +75,6 @@ export default function Navbar() {
         </div>
       </div>
     </div>
-    <div className="test"></div>
     </>
   );
 }
